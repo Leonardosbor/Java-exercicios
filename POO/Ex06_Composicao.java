370cdda4ee220756e642889ea903564961f3b4c5
Leonardo Reis <reis8191@gmail.com>
2026-08-14 15:56:03 -0300

    Composição em POO Java
    Prática de exercício com foco em Composição, aplicando também os conceitos de Getter, Método e Construtor.

--- POO/Ex06_Composicao.java
+++ POO/Ex06_Composicao.java
@@ -3,7 +3,7 @@ class Endereco{
     private String cidade;
     private String estado;
 
-    Endereco(String rua, String cidade, String estado){
+    public Endereco(String rua, String cidade, String estado){
         this.rua = rua;
         this.cidade = cidade;
         this.estado = estado;
@@ -27,7 +27,7 @@ class Cliente{
     private String nome;
     private Endereco endereco;
 
-    Cliente(String nome, Endereco endereco){
+    public Cliente(String nome, Endereco endereco){
         this.nome = nome;
         this.endereco = endereco;
     }
@@ -39,17 +39,30 @@ class Cliente{
     public Endereco getEndereco(){
         return endereco;
     }
+
+    public void mostrarDadosCliente(){
+        System.out.println();
+        System.out.println("=== RESUMO DADOS CLIENTE ===");
+        System.out.println("Nome: " + getNome());
+        System.out.println("Rua: " + getEndereco().getRua());
+        System.out.println("Cidade: " + getEndereco().getCidade());
+        System.out.println("Estado: " + getEndereco().getEstado());
+        System.out.println();
+
+    }
 }
 
 public class Ex06_Composicao {
     public static void main(String[] args) {
         
+        Endereco endereco = new Endereco(
+            "Rua Sebastião Alzemiro dos Santos", "Palhoça", 
+            "Santa Catarina"
+        );
+
+        Cliente cliente = new Cliente("Leonardo", endereco);
+
+        cliente.mostrarDadosCliente();
+
     }
 }
-
-
-
-
-
-
-
